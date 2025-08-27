9e5cde1e81a2216adcba67a7c2c5e2ccfc8d58c1
Thiago Contelli <thiagocontellid@gmail.com>
2025-08-26 21:02:24 -0300

    Add project files.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "UserManagement")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "UserManagement.Pages")]
