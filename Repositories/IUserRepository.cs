273565d976060e24d4515b48185f8659993a1e90
Thiago Contelli <thiagocontellid@gmail.com>
2025-08-27 22:34:19 -0300

    Add remaining CRUD operations to in-memory repository

--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -5,4 +5,8 @@ namespace UserManagement.Repositories;
 public interface IUserRepository
 {
     Task<List<User>> GetUsers();
+    Task AddUser(User user);
+    Task UpdateUser(User user);
+    Task<User?> GetUserById(Guid id);
+    Task DeleteUser(Guid id);
 }
--- Repositories/InMemoryUserRepository.cs
+++ Repositories/InMemoryUserRepository.cs
@@ -6,9 +6,48 @@ public class InMemoryUserRepository : IUserRepository
 {
     private readonly List<User> users = [];
 
+    public async Task AddUser(User user)
+    {
+        await Task.Delay(500);
+
+        User? existingUser = await GetUserById(user.Id);
+
+        if (existingUser is not null)
+        {
+            throw new Exception("User already exists");
+        }
+
+        users.Add(user);
+    }
+
+    public async Task DeleteUser(Guid id)
+    {
+        await Task.Delay(500);
+        
+        User? user = await GetUserById(id) ?? throw new Exception("User not found");
+
+        users.Remove(user);
+    }
+
+    public async Task<User?> GetUserById(Guid id)
+    {
+        await Task.Delay(500);
+        return users.FirstOrDefault(u => u.Id == id);
+    }
+
     public async Task<List<User>> GetUsers()
     {
-        await Task.Delay(1500);
+        await Task.Delay(500);
         return users;
+    }
+
+    public async Task UpdateUser(User user)
+    {
+        await Task.Delay(500);
+
+        User? existingUser = await GetUserById(user.Id) ?? throw new Exception("User not found");
+
+        users.Remove(existingUser);
+        users.Add(user);
     }
 }
